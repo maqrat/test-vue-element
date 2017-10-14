2d4df079550f335b37c688a907c8d8a1c99cb1c1
magrat <maqrat@gmail.com>
2017-10-14 19:20:03 +0700

    full ElUi import

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
-Vue.component(Button.name, Button)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
